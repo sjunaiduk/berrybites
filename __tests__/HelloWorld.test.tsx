8fb3c9162ba7d3052d0bbc4243195a81ffc335f0
Syed Junaid <sjunaiduk@gmail.com>
2024-02-29 20:21:44 +0000

    write a basic hello world test (not testing react components at this point)

---
+++ __tests__/HelloWorld.test.tsx
@@ -0,0 +1,6 @@
+import { describe, expect, it } from "vitest";
+describe("Hello World Test", () => {
+  it("Should return hello world", () => {
+    expect("Hello World").toEqual("Hello World");
+  });
+});
