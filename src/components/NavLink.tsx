0712c81701f9292c1547e41a371b1554aa8d4870
Syed Junaid <sjunaiduk@gmail.com>
2024-03-01 15:17:50 +0000

    install chakra ui react component library and create a navlink component

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,23 @@
+import { useColorModeValue, Box } from "@chakra-ui/react";
+
+interface Props {
+  children: React.ReactNode;
+}
+export const NavLink = (props: Props) => {
+  const { children } = props;
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
+    >
+      {children}
+    </Box>
+  );
+};
