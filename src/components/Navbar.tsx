6977ef4f25e99fc660199991643ac1a9d13a2ce5
Syed Junaid <sjunaiduk@gmail.com>
2024-03-01 16:06:45 +0000

    create styled navbar component. remove default css styling. change default logo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Button, HStack, Image, Text, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import logo from "../assets/berrybitelogo.svg";
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <HStack
+      justifyContent={"space-between"}
+      px={3}
+      py={3}
+      bg={`${colorMode == "dark" ? "#ff8000" : "white"}`}
+      color={`${colorMode == "dark" ? "white" : "#ff8000"}`}
+    >
+      <HStack spacing={0}>
+        <Image src={logo} />
+        <Text fontWeight={600} fontSize={"x-large"}>
+          BerryBite
+        </Text>
+      </HStack>
+      <Button
+        className={`${colorMode == "dark" ? "moon-icon" : "sun-icon"}`}
+        onClick={() => toggleColorMode()}
+        bg={"#ff9e007d"}
+      >
+        {colorMode == "dark" ? <MoonIcon /> : <SunIcon />}
+      </Button>
+    </HStack>
+  );
+};
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import { ChakraBaseProvider, extendTheme } from "@chakra-ui/react";
+const theme = extendTheme({
+  config: {
+    disableTransitionOnChange: false,
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
   </React.StrictMode>
 );
