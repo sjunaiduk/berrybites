5ebf34ab49740b40801725ebfef434dcdca6f5a0
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 15:06:35 +0000

    add state to searchbar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,34 +4,43 @@ import {
   InputLeftElement,
   Button,
   InputRightElement,
+  FormControl,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 function SearchBar() {
+  const [postcode, setPostcode] = useState("");
   return (
-    <InputGroup mt={8} size="lg">
-      <InputLeftElement>
-        <SearchIcon />
-      </InputLeftElement>
-      <Input
-        placeholder="Search restaurants by postcode"
-        textOverflow={"ellipsis"}
-        pr={{
-          base: "7rem",
-          md: "10rem",
-        }}
-      />
-      <InputRightElement width="6rem">
-        <Button
-          _hover={{ bg: "#ff9000" }}
-          backgroundColor={"#ff8000"}
-          borderRadius={20}
-          color={"white"}
-        >
-          Search
-        </Button>
-      </InputRightElement>
-    </InputGroup>
+    <FormControl isInvalid={false}>
+      <InputGroup mt={8} size="lg">
+        <InputLeftElement>
+          <SearchIcon />
+        </InputLeftElement>
+        <Input
+          placeholder="Search restaurants by postcode"
+          textOverflow={"ellipsis"}
+          value={postcode}
+          onChange={(e) => setPostcode(e.target.value)}
+          pr={{
+            base: "7rem",
+            md: "10rem",
+          }}
+        />
+        <InputRightElement width="6rem">
+          <Button
+            _hover={{ bg: "#ff9000" }}
+            backgroundColor={"#ff8000"}
+            borderRadius={20}
+            color={"white"}
+          >
+            Search
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <FormErrorMessage>Email is required.</FormErrorMessage>
+    </FormControl>
   );
 }
 
