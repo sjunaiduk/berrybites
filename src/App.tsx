3ac574872dfda7f845b9644dfdc6bc2fb2ec8ec0
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 01:41:06 +0000

    put container component in app.tsx to serve as 'layout' page. this will center the main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Container, Flex } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
+import RestaurantsGrid from "./components/RestaurantsGrid";
+import { HardcodedRestaurants } from "./utils";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Container maxWidth={"800px"}>
+        <Flex justifyContent={"center"}>
+          <RestaurantsGrid restaurants={HardcodedRestaurants} />
+        </Flex>
+      </Container>
+    </>
+  );
 }
 
 export default App;
