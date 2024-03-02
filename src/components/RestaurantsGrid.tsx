fda14068736135423d40c5f0f345b974c347f19e
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 01:41:52 +0000

    create restarant grid component to display restaurants

---
+++ src/components/RestaurantsGrid.tsx
@@ -0,0 +1,24 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import RestaurantCard from "./RestaurantCard";
+
+interface Props {
+  restaurants: Restaurant[];
+}
+const RestaurantsGrid = ({ restaurants }: Props) => {
+  return (
+    <SimpleGrid
+      mt={"4rem"}
+      spacing={5}
+      templateColumns={{
+        base: "1fr",
+        xlg: "1fr 1fr",
+      }}
+    >
+      {restaurants.map((restaurant) => (
+        <RestaurantCard restaurant={restaurant} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default RestaurantsGrid;
