d868c4fd5b40bc8ab851313d12292b916c4d0221
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 16:14:27 +0000

    store justeat restaurants endpoint in env variable

--- __tests__/SearchBar.test.tsx
+++ __tests__/SearchBar.test.tsx
@@ -5,20 +5,26 @@ import userEvent from "@testing-library/user-event";
 
 describe("Search bar", () => {
   afterEach(() => cleanup());
-  it("Should show an error message if the entered postcode is invalid when search is clicked", async () => {
-    // Arrange
-    const user = userEvent.setup();
-    render(<SearchBar />);
-    var searchBar = await screen.queryByTestId("postcode-searchbar");
-    var searchButton = await screen.queryByTestId("postcode-search-button");
 
-    // Act
-    await user.type(searchBar as HTMLElement, "123");
-    await user.click(searchButton as HTMLElement);
+  it.each(["123", "ABC", "acbscabdjals;lasd"])(
+    "Should show an error message if the entered postcode is invalid when search is clicked",
+    async (userInput: string) => {
+      // Arrange
+      const user = userEvent.setup();
+      render(<SearchBar />);
+      var searchBar = await screen.queryByTestId("postcode-searchbar");
+      var searchButton = await screen.queryByTestId("postcode-search-button");
 
-    // Assert
-    expect(screen.queryByTestId("searchbar-error-message")).toBeInTheDocument();
-  });
+      // Act
+      await user.type(searchBar as HTMLElement, userInput);
+      await user.click(searchButton as HTMLElement);
+
+      // Assert
+      expect(
+        screen.queryByTestId("searchbar-error-message")
+      ).toBeInTheDocument();
+    }
+  );
 
   it("Should NOT show error message if the entered postcode is valid when search is clicked", async () => {
     // Arrange
