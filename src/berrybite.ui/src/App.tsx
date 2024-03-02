d7d28b22b56706925f8b9b0324b45d944c0660b1
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 20:45:25 +0000

    add nock as dev dependency, return hardcoded restaraunts in PROD since in production nothing will run the proxy at the moment

--- src/berrybite.ui/src/App.tsx
+++ src/berrybite.ui/src/App.tsx
@@ -1,17 +1,18 @@
 import { Container, Flex } from "@chakra-ui/react";
 import Navbar from "./components/Navbar/Navbar";
 import RestaurantsGrid from "./components/RestaurantsGrid/RestaurantsGrid";
-import { HardcodedRestaurants } from "./utils/data";
 import SearchBar from "./components/SearchBar/SearchBar";
+import { useRestaurants } from "./hooks/useRestaurants";
 
 function App() {
+  const { data } = useRestaurants("LU11TU");
   return (
     <>
       <Navbar />
       <Container maxWidth={"800px"}>
         <Flex justifyContent={"center"} flexDirection={"column"}>
           <SearchBar />
-          <RestaurantsGrid restaurants={HardcodedRestaurants} />
+          <RestaurantsGrid restaurants={data || []} />
         </Flex>
       </Container>
     </>
--- src/berrybite.ui/src/services/restaurantService.ts
+++ src/berrybite.ui/src/services/restaurantService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { HardcodedRestaurants } from "../utils/data";
 
 const client = axios.create({
   baseURL: import.meta.env.VITE_JET_RESTAURANTS_API,
@@ -6,6 +7,11 @@ const client = axios.create({
 
 class RestaurantService {
   async GetAll(postcode: string): Promise<Restaurant[]> {
+    console.log("Environment is ", import.meta.env.MODE);
+
+    if (import.meta.env.MODE == "Production") {
+      return HardcodedRestaurants;
+    }
     var res = await client.get<GetRestaurantsResponse>(`/${postcode}`);
     return res.data.restaurants;
   }
