e9aa1c79865317d015a5d1fae193a0347e380278
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 03:13:37 +0000

    restyled restaurant card to better align the address with the top of rating, using grids instead of just flex.

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -8,6 +8,8 @@ import {
   Flex,
   Tag,
   SimpleGrid,
+  VStack,
+  Stack,
 } from "@chakra-ui/react";
 
 import { StarIcon } from "@chakra-ui/icons";
@@ -16,43 +18,92 @@ interface Props {
 }
 const RestaurantCard = ({ restaurant }: Props) => {
   return (
-    <Card px={4} py={6} background={"#fff"} color={"#000"}>
-      <HStack justifyContent={"space-around"} spacing={3}>
-        <HStack>
-          <Flex>
-            <Image src={restaurant.logoUrl} width={"100%"} />
-          </Flex>
-          <Box ml={4}>
-            <Heading size="lg" mb={7}>
-              {restaurant.name}
-            </Heading>
-
-            <Flex mt={3} alignItems={"center"}>
-              <StarIcon color={"orange"} mr={1} />{" "}
-              {restaurant.rating.starRating} stars
-            </Flex>
-
-            <SimpleGrid
-              mt={4}
-              width={"fit-content"}
-              spacing={1}
-              templateColumns="repeat(3, max-content)"
-            >
-              {restaurant.cuisines.map((cuisine, index) => (
-                <Tag key={index} size={"sm"} color={"gray"}>
-                  {cuisine.name}
-                </Tag>
-              ))}
-            </SimpleGrid>
-          </Box>
-        </HStack>
-
-        <Flex flexDirection={"column"} mt={"3rem"}>
-          <Text>{restaurant.address.firstLine}</Text>
-          <Text>{restaurant.address.city}</Text>
-          <Text>{restaurant.address.postalCode}</Text>
+    <Card minW={"320px"} px={4} py={6} background={"#fff"} color={"#000"}>
+      <SimpleGrid
+        gap={{
+          base: 2,
+          sm: 10,
+        }}
+        templateColumns={{
+          base: "1fr",
+          sm: "auto 1fr",
+        }}
+      >
+        <Flex id="image" alignItems={"center"}>
+          <Image src={restaurant.logoUrl} />
         </Flex>
-      </HStack>
+        <Stack id="content" width={"100%"}>
+          <Heading size="lg" mb={7}>
+            {restaurant.name}
+          </Heading>
+          <SimpleGrid
+            templateColumns={{
+              base: "1fr",
+              sm: "auto 1fr",
+            }}
+          >
+            <VStack alignItems={"left"}>
+              <Flex alignItems={"center"}>
+                <StarIcon color={"orange"} mr={1} />{" "}
+                <Text fontWeight={"bold"} className="star-rating">
+                  {restaurant.rating.starRating}
+                </Text>
+              </Flex>
+
+              <SimpleGrid
+                mt={{
+                  base: 2,
+                  sm: 4,
+                }}
+                width={"fit-content"}
+                spacing={1}
+                templateColumns="repeat(3, max-content)"
+              >
+                {restaurant.cuisines.map((cuisine, index) => (
+                  <Tag key={index} size={"sm"} color={"gray"}>
+                    {cuisine.name}
+                  </Tag>
+                ))}
+              </SimpleGrid>
+            </VStack>
+
+            <Flex
+              alignItems={{
+                base: "auto",
+                sm: "flex-end",
+              }}
+              mt={{
+                base: 4,
+                sm: 0,
+              }}
+              width={"100%"}
+              flexDirection={"column"}
+            >
+              <Box
+                mr={{
+                  sm: 7,
+                }}
+              >
+                {" "}
+                <Flex
+                  flexDirection={{
+                    base: "row",
+                    sm: "column",
+                  }}
+                  gap={1}
+                  wrap={"wrap"}
+                  fontSize={"sm"}
+                  color={"grey"}
+                >
+                  <Text>{restaurant.address.firstLine}</Text>
+                  <Text>{restaurant.address.city}</Text>
+                  <Text>{restaurant.address.postalCode}</Text>
+                </Flex>
+              </Box>
+            </Flex>
+          </SimpleGrid>
+        </Stack>
+      </SimpleGrid>
     </Card>
   );
 };
