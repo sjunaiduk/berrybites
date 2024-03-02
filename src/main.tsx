1d5ad134d9a9c1e960fa98e32027897de730030e
Syed Junaid <sjunaiduk@gmail.com>
2024-03-02 18:17:32 +0000

    install @tanstack/react-query and wrap react application with it

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ChakraBaseProvider, extendTheme } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 const theme = extendTheme({
   config: {
     disableTransitionOnChange: false,
@@ -11,7 +14,9 @@ const theme = extendTheme({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraBaseProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraBaseProvider>
   </React.StrictMode>
 );
