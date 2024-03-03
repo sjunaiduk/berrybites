7a9190a9d8c28e2ed83bcb39bd93aa188717d3c1
Syed Junaid <sjunaiduk@gmail.com>
2024-03-03 18:56:22 +0000

    remove theme toggle button, change navbar color

--- src/berrybite.ui/src/components/Navbar/Navbar.test.tsx
+++ src/berrybite.ui/src/components/Navbar/Navbar.test.tsx
@@ -1,8 +1,6 @@
 import { afterEach, describe, expect, it } from "vitest";
-import { cleanup, render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { cleanup, render, waitFor } from "@testing-library/react";
 import Navbar from "./Navbar";
-import { ChakraUiWrapper } from "../../../__tests__/utils";
 
 describe("Navbar", () => {
   afterEach(() => cleanup());
@@ -12,24 +10,5 @@ describe("Navbar", () => {
 
     // Assert
     await waitFor(() => expect(queryByText("BerryBite")).toBeInTheDocument());
-  });
-
-  it("should change the class of toggle theme when toggle theme button is clicked", async () => {
-    // Arrange
-    const user = userEvent.setup();
-    render(
-      <ChakraUiWrapper>
-        <Navbar />
-      </ChakraUiWrapper>
-    );
-    const toggleThemeButton = await screen.queryByTestId("toggle-theme");
-    var classesBefore = toggleThemeButton?.classList.value;
-
-    // Act
-    await user.click(toggleThemeButton as HTMLElement);
-    var classesAfter = toggleThemeButton?.classList.value;
-
-    // Assert
-    expect(classesBefore).not.toEqual(classesAfter);
   });
 });
